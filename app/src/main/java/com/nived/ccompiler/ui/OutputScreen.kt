9bed58531057a7fdcaf8e697644fcc74c31018b0
Nivedck <achuchakkara19@gmail.com>
2025-02-24 22:51:18 +0530

    output screen ui added

---
+++ app/src/main/java/com/nived/ccompiler/ui/OutputScreen.kt
@@ -0,0 +1,15 @@
+package com.example.myccompilerapp.ui
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun OutputScreen(output: String) {
+    Card(elevation = 4.dp, modifier = Modifier.fillMaxWidth().height(150.dp)) {
+        Text(text = output, modifier = Modifier.padding(16.dp))
+    }
+}
